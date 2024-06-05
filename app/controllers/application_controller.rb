e6d1ecd958eb795b0ff3149a686f4b70ebfe5141
luvo sapa <sapaluvo@gmail.com>
2024-06-05 09:52:31 +0200

    Making water tracker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,4 @@
 class Day < ApplicationRecord
   belongs_to :user
+  has_many :water_trackers
 end
