ade08312abf7bf3d38adf38e7a44c8c8066f9d92
Nolusindiso Ncamiso <nncamiso30108@gmail.com>
2024-06-05 16:32:00 +0200

    fixing

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,5 @@
 class Schedule < ApplicationRecord
   belongs_to :day
   has_many :schedule_tasks
-  validates :date, presence: true, uniqueness: { scope: :user_id }
+  validates :date, presence: true, uniqueness: { scope: :day_id }
 end
