063462d5d61fc035f04a8872038f55568bdf5c9a
Megan Street <meganjstreet@gmail.com>
2024-06-06 15:20:57 +0200

    Making navbar a bit less kak

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,16 +5,4 @@ import "@popperjs/core"
 import "bootstrap"
 import "@rails/request.js"
 
-import Rails from "@rails/ujs"
 
-Rails.start()
-
-// Ensure CSRF token is included in all AJAX requests
-document.addEventListener("turbolinks:load", () => {
-  const token = document.querySelector('meta[name="csrf-token"]').content;
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-Token': token
-    }
-  });
-});
