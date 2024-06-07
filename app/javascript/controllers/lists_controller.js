c71aa089937838e0a9051d42a74787e8f2d0ebf4
Nolusindiso Ncamiso <nncamiso30108@gmail.com>
2024-06-07 14:59:44 +0200

    whatever amazing work Nolu was doing before Kiki and Meg interrupted her

--- app/javascript/controllers/lists_controller.js
+++ app/javascript/controllers/lists_controller.js
@@ -4,17 +4,4 @@ import { Controller } from "@hotwired/stimulus"
     static targets = ["button", "link"]
   // [...]
 
-  () {
-    this.buttonTarget.innerText = "Bingo!";
-    this.buttonTarget.setAttribute("disabled", "");
-    this.linkTarget.classList.remove("d-none");
   }
-}
-
-const selectAllCheckbox = document.querySelector("#select-all-checkbox")
-selectAllCheckbox.addEventListener('click', (event) => {
-  const checkboxes = document.querySelectorAll('.form-check-input');
-  checkboxes.forEach((checkbox) => {
-    checkbox.checked = event.currentTarget.checked;
-  });
-})
