ffe53192954b82782e05d77d2259f9a03f74052e
Megan Street <meganjstreet@gmail.com>
2024-06-19 15:46:25 +0200

    Fixing syntax error, must stop pushing to master

--- app/models/water_tracker.rb
+++ app/models/water_tracker.rb
@@ -1,5 +1,5 @@
 class WaterTracker < ApplicationRecord
   belongs_to :day
   enum status: { incomplete: 0, complete: 1 }
-  validates uniqueness: { scope: :day_id }
+  validates_uniqueness_of :day_id
 end
