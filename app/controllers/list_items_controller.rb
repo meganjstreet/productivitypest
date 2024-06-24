61390c926e3e95725ddf46da57d91449df6fda02
Megan Street <meganjstreet@gmail.com>
2024-06-24 14:46:19 +0200

    Making list item editable

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -41,7 +41,21 @@ class ListItemsController < ApplicationController
   def update
     @list_item = ListItem.find(params[:id])
     @list_item.update(list_item_params)
-    @list_item.save!
+    @list = @list_item.list
+
+    respond_to do |format|
+      if @list_item.save
+        partial_html = render_to_string(partial: "lists/list_items",
+                                        locals: { list: @list, new_list_item: ListItem.new },
+                                        formats: [:html])
+        format.json { render json: { partial_html: partial_html }, status: :ok }
+      else
+        partial_html = render_to_string(partial: "lists/list_items",
+                                        locals: { list: @list, new_list_item: @list_item },
+                                        formats: [:html])
+        format.json { render json: { partial_html: partial_html, errors: @list_item.errors.full_messages }, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
