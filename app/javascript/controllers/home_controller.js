9639779e6a8ffda57c3b2c449c122d7603637e42
Megan Street <meganjstreet@gmail.com>
2024-06-25 09:23:44 +0200

    making meditation icon white when active

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -11,76 +11,76 @@ export default class extends Controller {
   toggleMeditations(){
 
     this.listsTarget.classList.remove("hidden");
-    this.listsBtnTarget.classList.remove("text-white");
+    this.listsBtnTarget.classList.remove("feature-active");
 
     this.streaksTarget.classList.remove("hidden");
-    this.streaksBtnTarget.classList.remove("text-white");
+    this.streaksBtnTarget.classList.remove("feature-active");
 
     this.waterTrackerTarget.classList.remove("hidden");
-    this.waterTrackerBtnTarget.classList.remove("text-white");
+    this.waterTrackerBtnTarget.classList.remove("feature-active");
 
     this.listsTarget.classList.add("hidden");
     this.streaksTarget.classList.add("hidden");
     this.waterTrackerTarget.classList.add("hidden");
 
     this.meditationsTarget.classList.toggle("hidden");
-    this.meditationsBtnTarget.classList.toggle("text-white");
+    this.meditationsBtnTarget.classList.toggle("feature-active");
 
   }
 
   toggleLists(){
     this.meditationsTarget.classList.remove("hidden");
-    this.meditationsBtnTarget.classList.remove("text-white");
+    this.meditationsBtnTarget.classList.remove("feature-active");
 
     this.streaksTarget.classList.remove("hidden");
-    this.streaksBtnTarget.classList.remove("text-white");
+    this.streaksBtnTarget.classList.remove("feature-active");
 
     this.waterTrackerTarget.classList.remove("hidden");
-    this.waterTrackerBtnTarget.classList.remove("text-white");
+    this.waterTrackerBtnTarget.classList.remove("feature-active");
 
     this.meditationsTarget.classList.add("hidden");
     this.streaksTarget.classList.add("hidden");
     this.waterTrackerTarget.classList.add("hidden");
 
     this.listsTarget.classList.toggle("hidden");
-    this.listsBtnTarget.classList.toggle("text-white")
+    this.listsBtnTarget.classList.toggle("feature-active")
 
   }
 
   toggleStreaks(){
     this.listsTarget.classList.remove("hidden");
-    this.listsBtnTarget.classList.remove("text-white");
+    this.listsBtnTarget.classList.remove("feature-active");
 
     this.meditationsTarget.classList.remove("hidden");
-    this.meditationsBtnTarget.classList.remove("text-white");
+    this.meditationsBtnTarget.classList.remove("feature-active");
 
     this.waterTrackerTarget.classList.remove("hidden");
-    this.waterTrackerBtnTarget.classList.remove("text-white");
+    this.waterTrackerBtnTarget.classList.remove("feature-active");
 
     this.listsTarget.classList.add("hidden");
     this.meditationsTarget.classList.add("hidden");
     this.waterTrackerTarget.classList.add("hidden");
 
     this.streaksTarget.classList.toggle("hidden");
-    this.streaksBtnTarget.classList.toggle("text-white");
+    this.streaksBtnTarget.classList.toggle("feature-active");
   }
 
   toggleWaterTracker(){
     this.listsTarget.classList.remove("hidden");
-    this.listsBtnTarget.classList.remove("text-white");
+    this.listsBtnTarget.classList.remove("feature-active");
 
     this.streaksTarget.classList.remove("hidden");
-    this.streaksBtnTarget.classList.remove("text-white");
+    this.streaksBtnTarget.classList.remove("feature-active");
 
     this.meditationsTarget.classList.remove("hidden");
-    this.meditationsBtnTarget.classList.remove("text-white");
+    this.meditationsBtnTarget.classList.remove("feature-active");
 
     this.listsTarget.classList.add("hidden");
     this.streaksTarget.classList.add("hidden");
     this.meditationsTarget.classList.add("hidden");
 
     this.waterTrackerTarget.classList.toggle("hidden");
-    this.waterTrackerBtnTarget.classList.toggle("text-white");
+    this.waterTrackerBtnTarget.classList.toggle("feature-active");
   }
 
 }
