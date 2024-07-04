6ec6e26517d17876beb168d2b440f00beee253ba
Megan Street <meganjstreet@gmail.com>
2024-07-04 10:40:12 +0200

    reverting back to see if meta image works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+ 
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -12,6 +13,6 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
-    end
+  end
 
 end
